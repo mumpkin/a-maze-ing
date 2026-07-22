191c590273d54008fb8a3553d30e1dc823888c02
Anthony MARIE-CALIXTE <anthonym.calixte@gmail.com>
2026-07-22 03:03:14 +0200

    [FIX]: a_maze_ing.py can be called from anywhere by using a virtual env located at the root of the repository

--- src/maze/maze_generator.py
+++ src/maze/maze_generator.py
@@ -1,7 +1,30 @@
 from typing import Any
-from abc import ABC
+from abc import ABC, abstractmethod
+
 
 
 class MazeGenerator(ABC):
-    def __init__(self, config: dict[str, Any]):
+    def __init__(self, config: dict[str, Any]) -> None:
+        self._config = config
+
+    def get_config_key(self, key: str) -> Any:
+        return self._config.get(key)
+
+    @abstractmethod
+    def build_maze(self) -> Any:
+        """
+        Builds the maze and displays the shortest route
+        between the entry point and the exit point
+        """
         pass
+
+    def write_output(self) -> None:
+        """
+        Writes the maze output into the filename
+        Specified in the configuration file 
+        """
+        try:
+            pass
+            # with open(self.get_config_key('OUTPUT_FILENAME'), 'w') as file:
+        except FileNotFoundError as fe:
+            print(fe)
