21e55a335215eb706547bb7add4bde7a88493124
amarie-c <amarie-c@f6r7s13.paris.42.school>
2026-08-17 11:26:34 +0200

    [CHORE]: modified main for testing purposes

--- src/a_maze_ing.py
+++ src/a_maze_ing.py
@@ -12,10 +12,11 @@ from utils import RenderEngine
 if __name__ == "__main__":
     """Main."""
     # seed(520)
-    print("-- CONFIG:", json.dumps(config.toJSON(), indent=4))
+    # print("-- CONFIG:", json.dumps(config.toJSON(), indent=4))
     maze = ImperfectMazeGenerator()
     maze.write_output()
     engine = RenderEngine(maze)
     maze.generate()
     engine.render()
+    maze.write_output()
     print()
