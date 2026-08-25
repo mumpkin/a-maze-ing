ef1f23d305b328d0f8886976b4de71d50120c439
Dimitri CHAUVEL <chauvel.dimitri@posteo.net>
2026-08-25 17:59:01 +0200

    Set title screen

---
+++ src/app/__init__.py
@@ -0,0 +1,5 @@
+"""App module."""
+
+from .app import App
+
+__all__ = ["App"]
---
+++ src/app/app.py
@@ -0,0 +1,131 @@
+"""App."""
+
+import os
+import subprocess
+from enum import Enum, auto
+
+import globals
+from maze import MazeGenerator, PerfectMazeGenerator
+from maze.imperfect_maze_generator import ImperfectMazeGenerator
+
+
+class AppState(Enum):
+    """."""
+
+    TitleScreen = auto()
+    Generator = auto()
+    Config = auto()
+
+
+class App:
+    """App definition."""
+
+    def __init__(self):
+        self.state: AppState = AppState.TitleScreen
+        self.generator: MazeGenerator = (
+            PerfectMazeGenerator()
+            if globals.config.perfect
+            else ImperfectMazeGenerator()
+        )
+
+    def run(self) -> None:
+        """Run the app."""
+        while True:
+            match self.state:
+                case AppState.TitleScreen:
+                    self._title()
+                case AppState.Generator:
+                    pass
+                case AppState.Config:
+                    pass
+
+    def _title(self):
+        _ = subprocess.run("clear")
+        print("\033[?25l", end="")
+        self._print_title()
+        self._route_title_action(self._get_user_input())
+
+    def _print_title(self):
+        """."""
+        term_width, term_height = os.get_terminal_size()
+        heading: list[str] = [
+            "\033[1m",
+            " _______     ___ ___ _______ _______ _______     ___ ______  _______ ",  # noqa: E501
+            "|   _   |   |   Y   |   _   |   _   |   _   |   |   |   _  \\|   _   |",  # noqa: E501
+            "|.  1   |   |.      |.  1   |___|   |.  1___|   |.  |.  |   |.  |___|",  # noqa: E501
+            "|.  _   |   |. \\_/  |.  _   |/  ___/|.  __)_    |.  |.  |   |.  |   |",  # noqa: E501
+            "|:  |   |   |:  |   |:  |   |:  1  \\|:  1   |   |:  |:  |   |:  1   |",  # noqa: E501
+            "|::.|:. |   |::.|:. |::.|:. |::.. . |::.. . |   |::.|::.|   |::.. . |",  # noqa: E501
+            "`--- ---'   `--- ---`--- ---`-------`-------'   `---`--- ---`-------'",  # noqa: E501
+            "\033[0m",
+        ]
+        options: list[str] = ["s - start", "c - show config", "q - quit"]
+        title_height: int = len(options) + len(heading)
+        title_width: int = len(max(heading, key=len))
+        if title_width >= term_width or title_height >= term_height:
+            self._wait_term_size(title_width, title_height)
+            term_width, term_height = os.get_terminal_size()
+        option_width: int = len(max(options, key=len))
+        fill_menu: str = "".join(
+            [" " for _ in range((term_width - option_width) // 2)]
+        )
+        for _ in range((term_height - len(heading) - len(options)) // 2):
+            print()
+        for line in heading:
+            print(line.center(term_width))
+        for line in options:
+            print(fill_menu + line)
+
+    def _route_title_action(self, action: str):
+        match action.lower():
+            case "s":
+                _ = subprocess.run("clear")
+                self.state = AppState.Generator
+            case "c":
+                _ = subprocess.run("clear")
+                self.state = AppState.Config
+            case "q":
+                _ = subprocess.run("clear")
+                exit(0)
+            case _:
+                pass
+
+    def _wait_term_size(self, width: int, height: int):
+        term_width, term_height = os.get_terminal_size()
+        while width >= term_width or height >= term_height:
+            _ = subprocess.run("clear")
+            print("\033[1m")
+            print("Adviced terminal size:".center(term_width))
+            print(f"{width}x{height}".center(term_width))
+            print("\033[0m")
+            print("return: retry - i: ignore - q: quit".center(term_width))
+            term_width, term_height = os.get_terminal_size()
+            user_input = self._get_user_input()
+            match user_input:
+                case "\n":
+                    continue
+                case "i":
+                    break
+                case "q":
+                    exit(0)
+                case _:
+                    pass
+        _ = subprocess.run("clear")
+
+    def _get_user_input(self) -> str:
+        import sys
+        import termios
+
+        fd = sys.stdin.fileno()
+        orig = termios.tcgetattr(fd)
+
+        new = termios.tcgetattr(fd)
+        new[3] = new[3] & ~termios.ICANON
+        new[6][termios.VMIN] = 1
+        new[6][termios.VTIME] = 0
+
+        try:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, new)
+            return sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, orig)
