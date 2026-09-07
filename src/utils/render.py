aa55b88fcedb8ce177a2606bf6213a82ec1089c9
Dimitri CHAUVEL <chauvel.dimitri@posteo.net>
2026-09-07 14:34:17 +0200

    Refacto Render docstrings

--- src/utils/render.py
+++ src/utils/render.py
@@ -9,63 +9,64 @@ from enums import CellState, ColorScheme, Compass
 
 @final
 class RenderEngine:
-    """Engine to render da maze."""
+    """Engine to render da maze.
 
-    def __init__(self, maze: maze.MazeGenerator) -> None:
+    Attributes
+    ----------
+    maze_generator : MazeGenerator
+        Instance of MazeGenerator to be rendered.
+    color_scheme : ColorScheme
+        Chosen color scheme to render the generated maze.
+
+    Methods
+    -------
+    render
+        Render the maze in the terminal.
+    """
+
+    def __init__(self, maze_generator: maze.MazeGenerator) -> None:
         """Render default constructor."""
-        self.maze = maze
-        self._color_scheme: ColorScheme = ColorScheme()
+        self.maze = maze_generator
+        self.color_scheme: ColorScheme = ColorScheme()
+
+    def render(self) -> None:
+        """Render the maze in the terminal."""
+        row = []
+        for y in range(globals.config.height):
+            row = [cell for cell in self.maze.grid if cell.pos.y == y]
+            self._draw_wall_row(row)
+            self._draw_cells_row(row)
+        self._draw_wall_row()
 
     def _get_tile_color(self, cell: maze.Cell) -> str:
-        """
-        Get tile color according to a cell attributes.
-
-        Keyword parameters:
-        cell: Cell -- A cell lul (╯°□°)╯︵ ┻━┻.
-        """
         match cell.pos:
             case globals.config.entry:
-                return self._color_scheme.ENTRY
+                return self.color_scheme.ENTRY
             case globals.config.exit:
-                return self._color_scheme.EXIT
+                return self.color_scheme.EXIT
             case _:
                 pass
         if cell in self.maze.optimal_path:
-            return self._color_scheme.OPTIMAL_PATH
+            return self.color_scheme.OPTIMAL_PATH
         match cell.state:
             case CellState.LOCKED:
-                return self._color_scheme.LOCKED
+                return self.color_scheme.LOCKED
             case CellState.IDLE:
-                return self._color_scheme.IDLE
+                return self.color_scheme.IDLE
             case CellState.VISITED:
-                return self._color_scheme.VISITED
+                return self.color_scheme.VISITED
             case CellState.VISITING:
-                return self._color_scheme.VISITING
-
-    def set_color_scheme(self, color_scheme: ColorScheme) -> None:
-        """."""
-        self._color_scheme = color_scheme
-
-    def get_color_scheme(self) -> ColorScheme:
-        """."""
-        return self._color_scheme
+                return self.color_scheme.VISITING
 
     def _draw_tile(self, color: str | None = None) -> None:
-        """
-        Draw the tile.
-
-        Keyword parameters:
-        color: TileColor -- Color used by the terminal.
-        """
         tile: str = "  "
         if color:
             print(color + tile, end="")
         else:
-            print(self._color_scheme.IDLE + tile, end="")
+            print(self.color_scheme.IDLE + tile, end="")
 
     def _draw_eol(self) -> None:
-        """Draw the end of line."""
-        print(self._color_scheme.TRANSPARENT)
+        print(self.color_scheme.TRANSPARENT)
 
     def _draw_wall_row(self, row: list[maze.Cell] | None = None) -> None:
         self._draw_tile()
@@ -94,7 +95,7 @@ class RenderEngine:
                         cell.pos == globals.config.entry
                         or cell.pos == globals.config.exit
                     ):
-                        self._draw_tile(self._color_scheme.VISITED)
+                        self._draw_tile(self.color_scheme.VISITED)
                     else:
                         self._draw_tile(self._get_tile_color(cell))
 
@@ -108,12 +109,6 @@ class RenderEngine:
         self._draw_eol()
 
     def _draw_cells_row(self, row: list[maze.Cell]) -> None:
-        """
-        Draw row of cells in the terminal.
-
-        Keyword parameters:
-        row: list[Cell] -- Row of cells to draw.
-        """
         self._draw_tile()
         for cell in row:
             east_neighbour = cell.get_neighbours().get(Compass.EAST)
@@ -140,19 +135,10 @@ class RenderEngine:
                     cell.pos == globals.config.entry
                     or cell.pos == globals.config.exit
                 ):
-                    self._draw_tile(self._color_scheme.VISITED)
+                    self._draw_tile(self.color_scheme.VISITED)
                 else:
                     self._draw_tile(self._get_tile_color(cell))
 
             else:
                 self._draw_tile()
         self._draw_eol()
-
-    def render(self) -> None:
-        """Render the maze."""
-        row = []
-        for y in range(globals.config.height):
-            row = [cell for cell in self.maze.grid if cell.pos.y == y]
-            self._draw_wall_row(row)
-            self._draw_cells_row(row)
-        self._draw_wall_row()
